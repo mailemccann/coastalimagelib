6a14ae792100f20ef1767769b186aa8248db367f
Maile McCann <33263256+mailemccann@users.noreply.github.com>
2021-07-13 16:40:59 -0400

    Removed classes, changed to functions

--- imageproducts.py
+++ imageproducts.py
@@ -2,14 +2,13 @@ import numpy as np
 import cv2 as cv
 import sys
 
-class ImageStats():
-    """
-    This class takes in rectified images as arguments in addImage() 
-    then calculates and saves statistical image products such as 
-    time- exposure, variance, brightest pixels, and darkest pixels.
+def imageStats(im_mat, save_flag=0):
+    '''
+    This function generates statistical image products for a given set of 
+    images and corresponding extrinsics/intrinsics. The statistical image
+    products are the timex, brightest, variance, darkest. 
 
-    All images must be in black and white, and must all have the
-    same dimensions.  
+    All images must have the same dimensions.  
 
     Attributes:
         Bright: brightest pixels at each location across the 
@@ -20,60 +19,24 @@ class ImageStats():
         Variance: standard deviation of all pixels at each location
                 across the collection
 
-    """
-    def __init__(self, images):
-        self.calc_flag = 0
-        self.im_mat = images
+    Args:
+        save_flag: flag to indicate if products should be saved
+                automatically to the user's drive
 
-    def addImage(self,Im):
-        self.im_mat = np.dstack((self.im_mat,Im))
+    '''
 
-    def saveProducts(self):
-        '''
-        Function to save all image products
+    Dark = np.uint8(np.nanmin(im_mat,axis=2))
+    Bright = np.uint8(np.nanmax(im_mat,axis=2))
+    Timex = np.uint8(np.nanmean(im_mat, axis=2)) 
+    Variance = np.uint8(np.nanstd(im_mat, axis=2))
+    if save_flag: 
+        cv.imwrite('Darkest.jpg', Dark)
+        cv.imwrite('Brightest.jpg', Bright)
+        cv.imwrite('Timex.jpg', Timex)
+        cv.imwrite('Variance.jpg', Variance)
 
-        Can only be called after calc stats has run
-        '''
-        if self.calc_flag:
-            cv.imwrite('Darkest.jpg', self.Dark)
-            cv.imwrite('Brightest.jpg', self.Bright)
-            cv.imwrite('Timex.jpg', self.Timex)
-            cv.imwrite('Variance.jpg', self.Variance)
-            cv.waitKey(0) 
-
-        else: sys.exit('calcStats() must be called before products can be saved.')
-
-    def dispProducts(self):
-        '''
-        Function to display all image products
-
-        Can only be called after calc stats has run
-        '''
-
-        if self.calc_flag:
-            cv.imshow('Darkest', self.Dark)
-            cv.imshow('Brightest', self.Bright)
-            cv.imshow('Timex', self.Timex)
-            cv.imshow('Variance', self.Variance)
-            cv.waitKey(0) 
-
-        else: sys.exit('calcStats() must be called before products can be displayed.')
-
-    def calcStats(self, save_flag=0):
-        '''
-        This function generates statistical image products for a given set of 
-        images and corresponding extrinsics/intrinsics. The statistical image
-        products are the timex, brightest, variance, darkest. 
-
-        Args:
-            save_flag: flag to indicate if products should be saved
-                    automatically to the user's drive
-
-        '''
-
-        self.calc_flag = 1
-        self.Dark = np.uint8(np.nanmin(self.im_mat,axis=2))
-        self.Bright = np.uint8(np.nanmax(self.im_mat,axis=2))
-        self.Timex = np.uint8(np.nanmean(self.im_mat, axis=2)) 
-        self.Variance = np.uint8(np.nanstd(self.im_mat, axis=2))
-        if save_flag: self.saveProducts()
+    cv.imshow('Darkest', Dark)
+    cv.imshow('Brightest', Bright)
+    cv.imshow('Timex', Timex)
+    cv.imshow('Variance', Variance)
+    cv.waitKey(0)
--- pixelproducts.py
+++ pixelproducts.py
@@ -1,113 +1,100 @@
 import numpy as np
 import cv2 as cv
 from scipy.interpolate import RegularGridInterpolator as reg_interp
-from corefuncs import Rectifier, CameraData
+import corefuncs as cf
 import imageio
 import matplotlib.pyplot as plt
+import sys
 
-
-class PixelStack(Rectifier):
-
+def pixelStack(grid, im_mat, intrinsic_list, extrinsic_list, sample_freq=1, disp_flag=0, coords = 'local', origin = 'None', mType = 'CIRN'):        
     '''
-    Class that contains pixel instruments for use in bathymetric inversion,
+    Function that creates pixel instruments for use in bathymetric inversion,
     surface current, run-up calculations, or other quantitative analyses. 
     Instruments can be made in both world and a local rotated coordinate
     system. However, for ease and accuracy, if planned to use in bathymetric
     inversion, surface current, or run-up applications, it should occur
     in local coordinates.
 
-    Args:
-        xlims (ndarray): min and max (inclusive) in the x-direction (e.g. [-250, 500])
-        ylims (ndarray): min and max (inclusive) in the y-direction (e.g. [0, 1800])
-        dx (float): resolution of grid in x direction (same units as camera calibration)
-        dy (float): resolution of grid in y direction (same units as camera calibration)
-        z (float): estimated elevation at every point in the x, y grid
-        coords (string): 'geo' or 'local'; if 'geo', extrinsics are transformed to local
-                but origin is needed to transform
+
+    Inputs:
+        grid: XYZGrid object of real world coordinates
+        cameras_images (array): N x M x K x num_frames struct of images, one image per camera (K) at the desired timestamp
+                        for rectification (N x M is image height x image width, k is number of cameras, num_frames is 
+                        number of frames/ timestamps)
+        intrinsic_list (list): 1x11xK internal calibration data for each camera
+        extrinsic_list (list): 1x6xK external calibration data for each camera
+        xyz (ndarray): 3xN array if desired pixels are from different points than the main XYZ grid 
+        sample_freq (int): desired frequency at which to grab pixels. This does not factor in camera metadata. User must know frames
+                        per second of collection. freq=1 means grab pixels at every frame. freq=2 means grab pixels at every other frame,
+                        and so on
+        disp_flag (bool): flag that user can use to display output image products
+            coords (string): 'geo' or 'local'; if 'geo', extrinsics are transformed to local
+            but origin is needed to transform
         origin: local origin (x,y,z,angle)
         mType (string): format of intrinsic matrix, 'CIRN' is default, 'DLT' is also supported
-
-    '''   
-
-    def __init__(self, xlims, ylims, dx=1, dy=1, z=0, coords = 'local', origin = 'None', mType = 'CIRN'):
-
-        Rectifier.__init__(self, xlims, ylims, dx, dy, z, coords, origin, mType)
-
-    def pullTransect(self, im_mat, intrinsic_list, extrinsic_list, sample_freq=1, disp_flag=0):        
-        '''
-        This function generates pixel instruments (timestacks) for a given set of images and
-        corresponding extrinsics/intrinsics. 
-
-        Inputs:
-            cameras_images (array): N x M x K x num_frames struct of images, one image per camera (K) at the desired timestamp
-                            for rectification (N x M is image height x image width, k is number of cameras, num_frames is 
-                            number of frames/ timestamps)
-            intrinsic_list (list): 1x11xK internal calibration data for each camera
-            extrinsic_list (list): 1x6xK external calibration data for each camera
-            xyz (ndarray): 3xN array if desired pixels are from different points than the main XYZ grid 
-            sample_freq (int): desired frequency at which to grab pixels. This does not factor in camera metadata. User must know frames
-                            per second of collection. freq=1 means grab pixels at every frame. freq=2 means grab pixels at every other frame,
-                            and so on
-            disp_flag (bool): flag that user can use to display output image products
-            
-        Returns:
-            pixels (ndarray): pixel intensities at xyz points (georectified image). Size depends on type of pixels. Axis 3 has the same length
-                            as number of frames/ sample rate
-
-        '''  
         
-        # Loop for Collecting Pixel Instrument Data.
-        num_frames = im_mat.shape[3]
+    Returns:
+        pixels (ndarray): pixel intensities at xyz points (georectified image). Size depends on type of pixels. Axis 3 has the same length
+                        as number of frames/ sample rate
 
-        for curr_frame in range(0,num_frames,sample_freq):
-            # Array for pixel values
-            self.numcams = len(intrinsic_list)
-            IrIndv = np.zeros((self.s[0], self.s[1], self.numcams))
-            if disp_flag: fig,axs = plt.subplots(1,self.numcams)
+    '''  
+    
+    # Loop for Collecting Pixel Instrument Data.
+    num_frames = im_mat.shape[3]
+    s = grid.X.shape
 
-            # Iterate through each camera from to produce single merged frame
-            for k, (intrinsics, extrinsics) in enumerate(zip(intrinsic_list, extrinsic_list)):
-                image = im_mat[:,:,k,curr_frame]
+    for curr_frame in range(0,num_frames,sample_freq):
+        # Array for pixel values
+        numcams = len(intrinsic_list)
+        IrIndv = np.zeros((s[0], s[1], numcams))
+        if disp_flag: fig,axs = plt.subplots(1,numcams)
 
-                # Load individual camera intrinsics, extrinsics, and camera matrices
-                self.calib = CameraData(intrinsics, extrinsics, self.origin, self.coords, self.mType)
+        # Iterate through each camera from to produce single merged frame
+        for k, (intrinsics, extrinsics) in enumerate(zip(intrinsic_list, extrinsic_list)):
+            image = im_mat[:,:,k,curr_frame]
 
-                # Work in grayscale
-                if len(image.shape) > 2:
-                    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  
+            # Load individual camera intrinsics, extrinsics, and camera matrices
+            calib = cf.CameraData(intrinsics, extrinsics, origin, coords, mType)
 
-                # Find distorted UV points at each XY location in grid
-                if self.mType == 'CIRN':
-                    self.Ud, self.Vd, flag = self.xyz2DistUV()
-                if self.mType == 'DLT':
-                    self.Ud, self.Vd = self.dlt2UV()
+            # Work in grayscale
+            if len(image.shape) > 2:
+                image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  
 
-                # Grab pixels from image at each position
-                ir = self.getPixels(image)
-                IrIndv[:,:,k] = ir
-
-                if disp_flag:
-                    # Show pixels on image
-                    axs[k].xaxis.set_visible(False)
-                    axs[k].yaxis.set_visible(False)
-                    axs[k].imshow(image.astype(np.uint8), cmap='gray', vmin=0, vmax=255)
-                    mask = (self.Ud<image.shape[1]) & (self.Ud>0) & (self.Vd<image.shape[0]) & (self.Vd>0)
-                    axs[k].scatter(self.Ud[mask], self.Vd[mask], s=3,c="r")
-                
-            if disp_flag: plt.show()
-                
-
-            # Merge rectifications of multiple cameras
-            IrIndv[IrIndv==0] = np.nan
-
-            # NEED BETTER BLENDING HERE
-            Ir = np.nanmean(IrIndv,axis=2)
-
-            if curr_frame == 0:   
-                pixels = Ir
+            # Find distorted UV points at each XY location in grid
+            if mType == 'CIRN':
+                Ud, Vd = cf.xyz2DistUV(grid, calib)
+            elif mType == 'DLT':
+                Ud, Vd = cf.dlt2UV(grid, calib)
             else:
-                pixels = np.dstack((pixels,Ir))
+                sys.exit('This intrinsics format is not supported')
 
-        # Return pixels
-        self.pixels = pixels.astype(np.uint8)
-        return self.pixels
+            # Grab pixels from image at each position
+            ir = cf.getPixels(image, Ud, Vd, s)
+            IrIndv[:,:,k] = ir
+
+            if disp_flag:
+                # Show pixels on image
+                axs[k].xaxis.set_visible(False)
+                axs[k].yaxis.set_visible(False)
+                axs[k].imshow(image.astype(np.uint8), cmap='gray', vmin=0, vmax=255)
+                mask = (Ud<image.shape[1]) & (Ud>0) & (Vd<image.shape[0]) & (Vd>0)
+                axs[k].scatter(Ud[mask], Vd[mask], s=3,c="r")
+            
+        if disp_flag: plt.show()
+            
+
+        # Merge rectifications of multiple cameras
+        IrIndv[IrIndv==0] = np.nan
+
+        # NEED BETTER BLENDING HERE
+        Ir = np.nanmean(IrIndv,axis=2)
+
+        if curr_frame == 0:   
+            pixels = Ir
+        else:
+            pixels = np.dstack((pixels,Ir))
+
+    # Return pixels
+    pixels = pixels.astype(np.uint8)
+
+    return pixels
