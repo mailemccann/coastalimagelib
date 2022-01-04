a5ec5a8d326d805aa2d4c8e9002fe37a451b3cf5
Maile McCann <mm2498@cornell.edu>
2022-01-04 11:06:11 -0800

    Updated getThreddsTideTp to default try EOP for tide

--- supportfunctions.py
+++ supportfunctions.py
@@ -365,26 +365,53 @@ def formatArgusFile(cams,folder,epoch, **kwargs):
 
 
 def getThreddsTideTp(t):
+    '''
+    Get peak period and water level from the FRF thredds server
 
+    Tries EOP first for water level, if doesn't work, uses 8m array
+
+    Args:
+        t: time in epoch (int)
+    Returns: 
+        Tp: Peak period (float)
+        WL: water level (float)
+    '''
     import datetime as dt
     from netCDF4 import Dataset
     
-    # Get peak period and water level from the thredds server
     time_obj = dt.datetime.utcfromtimestamp(int(t))
     hr  =time_obj.hour
     yr = time_obj.year
     mon_str = str(str(time_obj.month).zfill(2))
-    ds = Dataset("https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/oceanography/waves/8m-array/" + str(yr) + "/FRF-ocean_waves_8m-array_" + str(yr) + mon_str + ".nc",'r')
+
+    # Peak period Dataset
+    ds = Dataset("https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/oceanography/waves/8m-array/" + \
+                    str(yr) + "/FRF-ocean_waves_8m-array_" + str(yr) + mon_str + ".nc",'r')
     wave_Tp = (ds.variables["waveTp"][:])
-    waterlevel = (ds.variables["waterLevel"][:])
-    thredds_time = np.asarray(ds.variables["time"][:])
-    ind = np.abs(thredds_time - t).argmin()
+    thredds_time_Tp = np.asarray(ds.variables["time"][:])
+
+    # Water Level Dataset
+    try: 
+        # Try EOP
+        ds2 = Dataset("https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/oceanography/waterlevel/eopNoaaTide/" + \
+                    str(yr) + "/FRF-ocean_waterlevel_eopNoaaTide_" + str(yr) + mon_str + ".nc",'r')
+        waterlevel = (ds2.variables["waterLevel"][:])
+        thredds_time_WL = np.asarray(ds2.variables["time"][:])
+        print('Water level sourced from EOP')
+    except: 
+        # If no EOP, grab from 8m array
+        waterlevel = (ds.variables["waterLevel"][:])
+        thredds_time_WL = np.asarray(ds.variables["time"][:])
+        print('Water level sourced from 8m array')
+
+    ind_WL = np.abs(thredds_time_WL - t).argmin()
+    ind_Tp = np.abs(thredds_time_Tp - t).argmin()
 
     # Peak period
-    Tp = int(np.ceil(wave_Tp[ind]))
+    Tp = int(np.ceil(wave_Tp[ind_Tp]))
 
     # Water level
-    WL = round(waterlevel[ind],2)
+    WL = round(waterlevel[ind_WL],2)
 
     return Tp, WL
 
